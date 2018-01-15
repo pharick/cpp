28f3da0cf1e0414f4506c54a5c8ef6b6d096edc9
Артем <pharick@localhost.localdomain>
2018-01-15 23:48:11 +0300

    max2

--- 14-new-year/max2.cpp
+++ 14-new-year/max2.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main() {
+    short max1, max2, n;
+    cin >> max1 >> max2 >> n;
+    if (max2 > max1) {
+        short temp = max1;
+        max1 = max2;
+        max2 = temp;
+    }
+
+    while (n) {
+        if (n > max1) {
+            max2 = max1;
+            max1 = n;
+        } else if (n > max2) {
+            max2 = n;
+        }
+
+        cin >> n;
+    }
+
+    cout << max2 << endl;
     return 0;
 }
